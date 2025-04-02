14fa0a78fbef72d3afedc79c54d9c72c535a049e
Julian Oehler <120657764+julianxbloom@users.noreply.github.com>
2025-04-02 15:47:50 +0000

    a

--- myapp/app.js
+++ myapp/app.js
@@ -1,4 +1,4 @@
-//var createError = require('http-errors');
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/grid', gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
