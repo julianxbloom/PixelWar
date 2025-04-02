170377dddff92441d17f92d0e924d93e2f180d79
Julian Oehler <120657764+julianxbloom@users.noreply.github.com>
2025-04-02 15:35:57 +0000

    c

--- myapp/app.js
+++ myapp/app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+//var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
