e6ae2979ca33f865a66f5f3cf28b6c8feb64769b
Julian Oehler <120657764+julianxbloom@users.noreply.github.com>
2025-04-03 14:12:17 +0000

    modif login.js

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const { route } = require('./grid');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('login')
+});
+
+router.post('/', (req, res) => {
+  res.redirect('/grid')
 });
 
 module.exports = router;
