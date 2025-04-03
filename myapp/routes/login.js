57d7d4881a2fe644fe634cac3227897b8dac661c
Julian Oehler <120657764+julianxbloom@users.noreply.github.com>
2025-04-03 14:24:39 +0000

    c

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -2,11 +2,12 @@ var express = require('express');
 const { route } = require('./grid');
 var router = express.Router();
 
-/* GET home page. */
+/* GET request */
 router.get('/', (req, res) => {
   res.render('login')
 });
 
+/* POST request */
 router.post('/', (req, res) => {
   res.redirect('/grid')
 });
