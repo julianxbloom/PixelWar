3d823993f01edd277460dc5d35f7d6050d2f087c
schaman1 <153807071+schaman1@users.noreply.github.com>
2025-04-04 10:12:30 +0000

    Login

--- myapp/public/javascripts/login.js
+++ myapp/public/javascripts/login.js
@@ -1,14 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const Class = document.getElementById('class-select');
-    const Pseudo = document.getElementById('pseudo');
-    const NextPage = document.getElementById('NextPage');
-
-    /*NextPage.addEventListener('click', () => {
-        CurrentClass = Class.options[Class.selectedIndex].text;
-        CurrentPseudo = Pseudo.value + CurrentClass;
-        localStorage.setItem('name', CurrentPseudo);
-        window.location.href='grid.ejs';
-        
-    });*/
 });
--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -2,19 +2,27 @@ var express = require('express');
 const { route } = require('./grid');
 var router = express.Router();
 
+router.use(express.urlencoded({ extended: true }));
+
 /* GET request */
 router.get('/', (req, res) => {
-  res.render('login')
+  res.render('login',{Btn : ""});
 });
 
 /* POST request */
 router.post('/', (req, res) => {
 
-  //Classe = document.getElementById('class-select').options[Class.selectedIndex].text;
-  //alert(Classe);
+  const pseudo = req.body.pseudo+req.body.CurrentClass
+  if (pseudo == "JulianTG01"){
+    res.redirect('/grid');
+  }
+
+  else {
+    res.render('login',{Btn : "Wrong pseudo,Retry"});
+  }
 
   //Verif a faire pour savoir si bon compte
-  res.redirect('/grid')
+  
   
 });
 
