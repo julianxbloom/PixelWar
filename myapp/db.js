657e65f8ee4e05ad8ceb38641f267e14fd5801b5
Julian Oehler <120657764+julianxbloom@users.noreply.github.com>
2025-04-11 10:15:24 +0000

    c

--- myapp/db.js
+++ myapp/db.js
@@ -9,8 +9,18 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
+  con.query("CREATE DATABASE IF NOT EXISTS pixelwar", function (err, result) {
     if (err) throw err;
     console.log("Database created");
+  });
+  var user_creation = ```CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT,
+    username VARCHAR(100),
+    pixelAvailable INT DEFAULT 5,
+    lastTimeUpdate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)```
+  con.query(user_creation, function(err, result) {
+    if (err) throw err;
+    console.log("Table users created!")
   });
 });
