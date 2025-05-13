734d321fc6ded1520f41213265179339022876f4
Julian Oehler <120657764+julianxbloom@users.noreply.github.com>
2025-05-13 17:49:06 +0000

    c

--- myapp/app.js
+++ myapp/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
-//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gridRouter = require('./routes/grid').router;
 var loginRouter = require('./routes/login');
