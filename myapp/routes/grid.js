e2a008ee57720b38c096b7571abe859d5b80f00c
Julian Oehler <120657764+julianxbloom@users.noreply.github.com>
2025-05-15 14:26:54 +0000

    cqs

--- myapp/routes/grid.js
+++ myapp/routes/grid.js
@@ -23,7 +23,7 @@ function setSocketIo(socketIo) {
 
   // Configure socket.io events
   io.on('connection', (socket) => {
-    socket.on('dataPixel', (data) => {console.log(data.x);
+    socket.on('dataPixel', (data) => {
       socket.emit('pixelUpdate',{x:data.x,y:data.y,color:data.color});
       console.log('Pixel to update: ', data);
     });
