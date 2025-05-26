a14f8eb813811fe7fa37e9f7e28549a9325a47fc
schaman1 <153807071+schaman1@users.noreply.github.com>
2025-05-26 14:28:32 +0000

    TUTO done

--- myapp/public/javascripts/login-script.js
+++ myapp/public/javascripts/login-script.js
@@ -1,22 +1,25 @@
 const images = [
-    '/images/bgGridTel.png',
-    '/images/bgGridTel.png',
-    '/images/bgGridTel.png',
-    '/images/bgGridTel.png',
-    '/images/bgGridTel.png',
-    '/images/bgGridTel.png'
+    '/images/Pixel war.png',
+    '/images/Pixel war (1).png',
+    '/images/Pixel war (2).png',
+    '/images/Pixel war (3).png',
+    '/images/Pixel war (4).png',
+    '/images/Pixel war (5).png'
 ];
 
 let currentIndex = 0;
 
 const imgElem = document.getElementById('tutoImg');
 const nextBtn = document.getElementById('nextBtn');
+const wrap = document.getElementById('wrapper');
+const tutoWrapper = document.getElementById('tutoWrapper');
 
 nextBtn.addEventListener('click', () => {
     currentIndex++;
     if (currentIndex >= images.length) {
     // Le tuto est fini, on masque le tuto et on affiche le formulaire
-    alert("Tuto terminé !");
+        wrap.style.display = 'block';
+        tutoWrapper.style.display = 'none';
     } else {
     // Change l'image du tuto
     imgElem.src = images[currentIndex];
