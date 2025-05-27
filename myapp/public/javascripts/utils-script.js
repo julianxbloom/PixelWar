99d025954b68c0a20a443a6e679f7c7c1e0e7c21
schaman1 <153807071+schaman1@users.noreply.github.com>
2025-05-27 08:13:02 +0000

    c

--- myapp/public/javascripts/utils-script.js
+++ myapp/public/javascripts/utils-script.js
@@ -8,11 +8,8 @@ socket.on('reloadServeur', () => {
 btn = document.getElementById('BtnReload');
 
 if (pseudo){
+    btn.style.display = "flex";
     btn.addEventListener('click', function() {
     socket.emit("reload");
     });
 }
-else{
-    btn.style.display = "none";
-}
-
