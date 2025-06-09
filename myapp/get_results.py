6c5884996dcb76ac850679d606b9f9da2ecfb2a9
Julian Oehler <120657764+julianxbloom@users.noreply.github.com>
2025-06-09 12:12:04 +0000

    script résultats par classe csv

--- myapp/get_results.py
+++ myapp/get_results.py
@@ -1,16 +1,73 @@
 import mysql.connector
 
-def get_results():
+def singleUserDataTreatment(user: tuple):
+    if not user[0]:
+        return False
+    
+    num_classe = user[0][-2:]
+            
+    #classe = Autres
+    if num_classe == 'es':
+        classe = 'Autres'
+        username = user[0][:-6]
+        return username, user[1], classe
+    elif num_classe == 'fs':
+        classe = 'Profs'
+        username = user[0][:-5]
+        return username, user[1], classe
+    
+    # autres cas
+    classe = user[0][-4:-2]
+    if classe == 'GT':
+        classe = '2GT' + num_classe
+        username = user[0][:-5]
+        return username, user[1], classe
+    
+    elif classe in ['1G', 'TG']:
+        classe += num_classe
+        username = user[0][:-4]
+        if username == 'tim':
+            classe = 'TG02'
+        return username, user[1], classe
+    
+    else:
+        return False
+
+
+def getResults():
     with mysql.connector.connect(
         host="yamanote.proxy.rlwy.net",
         port=30831,
+        database="railway",
         user="root",
         password="yMdXBhOeslFOqRfhbbHUWUlijPQZtLlI"
-        ):
+        ) as conn:
         
-        print("connected")
+        cur = conn.cursor()
         
-        query = "SELECT username, pixelcolored FROM users"
+        query = "SELECT users, nbrColor FROM user"
+        cur.execute(query)
+        data = cur.fetchall()
         
+        results = []
+        
+        for user in data:
+            result = singleUserDataTreatment(user)
+            if result:
+                results.append(result)
+            
+        return results
 
-get_results()
+def resultatsParClasse():
+    resultatsIndiv = getResults()
+    
+    results = {}
+    
+    for user in resultatsIndiv:
+        if user[2] not in results:
+            results[user[2]] = user[1]
+        else:
+            results[user[2]] += user[1]
+    return results
+
+print(resultatsParClasse())
