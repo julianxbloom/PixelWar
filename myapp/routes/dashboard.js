87f16c2a4e2385e9577e68a12f2f5221e963b3de
Timothee <nobilettimothee@gmail.com>
2026-03-07 15:28:31 +0100

    All done properly

--- myapp/routes/dashboard.js
+++ myapp/routes/dashboard.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql2');
+
+require("dotenv").config();
 
 //router.use(express.urlencoded({ extended: true }));
 
-router.get('/', (req, res) => {
-    return res.render('dashboard');
-});
+var con = mysql.createPool(({
+  host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+  port: 4000,
+  // --- C'EST CETTE PARTIE QUI MANQUE ---
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }
 
+}))
+
+router.get('/', (req, res) => {
+    let text = "";
+    con.query('SELECT name,nbrPixel FROM team ORDER BY nbrPixel DESC',(err,result)=>{
+        if (err) throw err;
+
+        for (team of result){
+            text+= team.name + " : " + team.nbrPixel;
+            text+="<br>";
+        }
+
+        return res.render('dashboard',{
+            team_values:text,
+        });
+    });
+});
 
 
 module.exports = router;
