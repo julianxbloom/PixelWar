a22c235167f20f1e86bc80c509464ac027d8def8
Timothee <nobilettimothee@gmail.com>
2026-03-12 20:53:57 +0100

    try

--- myapp/routes/waiting.js
+++ myapp/routes/waiting.js
@@ -15,7 +15,7 @@ var con = mysql.createPool(({
   user:process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database:process.env.DB_NAME,
-  port: 4000,
+  port: process.env.DB_PORT,
   // --- C'EST CETTE PARTIE QUI MANQUE ---
   ssl: {
     minVersion: 'TLSv1.2',
